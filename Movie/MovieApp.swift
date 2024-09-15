ce548699bac5a5cc06382986a0903ee4eb078b82
rizaradiarivaldo <rizaradiarivaldo@gmail.com>
2024-09-15 21:34:27 +0700

    Initial Commit

---
+++ Movie/MovieApp.swift
@@ -0,0 +1,17 @@
+//
+//  MovieApp.swift
+//  Movie
+//
+//  Created by Riza Radia Rivaldo on 15/09/24.
+//
+
+import SwiftUI
+
+@main
+struct MovieApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
